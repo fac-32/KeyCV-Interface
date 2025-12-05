5aba542be831ecb8f8b8e2ec413af1df0f73e483
krilklem <rafi.panyotova@gmail.com>
2025-12-05 21:36:10 +0000

    feat: Sign up a new user in supabase using the form input

--- src/components/ui/Create-User.tsx
+++ src/components/ui/Create-User.tsx
@@ -4,32 +4,70 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 
-const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY);
-
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY,
+);
 
 const CreateUser = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
-    const submitHandler = async ( event ) => {
-        event.preventDefault();
-    };
+  const submitHandler = async (event) => {
+    event.preventDefault();
 
-    return (
-        <div className="grid w-full max-w-sm items-center gap-3">Create an account
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" id="email"
-                onChange={(event) => setEmail(event.target.value)}
-                placeholder="example@email.com" required/>
+    setMessage("");
 
-            <Label htmlFor="password">Password</Label>
-            <Input type="password" id="password" 
-                onChange={(event) => setPassword(event.target.value)}
-                required />
+    if (!email || !password) {
+      setMessage("email and password fields are required");
+      return;
+    }
 
-            <Button type="submit" onClick={submitHandler} variant="outline">Create</Button>
-        </div>
-    );
+    if (password.length < 8) {
+      setMessage("password must be at least 8 characters long");
+      return;
+    }
+
+    const { data, error } = await supabase.auth.signUp({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      setMessage(
+        "an error has occured while creating your account, please try again later",
+      );
+    }
+    if (data) {
+      setMessage("acount created");
+    }
+  };
+
+  return (
+    <div className="grid w-full max-w-sm items-center gap-3">
+      Create an account
+      <Label htmlFor="email">Email</Label>
+      <Input
+        type="email"
+        id="email"
+        onChange={(event) => setEmail(event.target.value)}
+        placeholder="example@email.com"
+        required
+      />
+      <Label htmlFor="password">Password</Label>
+      <Input
+        type="password"
+        id="password"
+        onChange={(event) => setPassword(event.target.value)}
+        required
+      />
+      <Button type="submit" onClick={submitHandler} variant="outline">
+        Create
+      </Button>
+      <p>{message}</p>
+    </div>
+  );
 };
 
 export default CreateUser;
