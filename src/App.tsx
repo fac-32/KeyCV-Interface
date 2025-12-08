a5d7ed6e573ce44e24e583ae36a6516af75ed1d5
krilklem <rafi.panyotova@gmail.com>
2025-12-08 16:35:25 +0000

    feat: Create login form UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import CVUpload from "./components/CV-Upload";
 import CreateUser from "./components/ui/Create-User";
+import LoginUser from "./components/ui/Login-User";
 
 function App() {
   return (
     <>
       <CreateUser />
+      <LoginUser />
       <CVUpload />
     </>
   );
---
+++ src/components/ui/Login-User.tsx
@@ -0,0 +1,29 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+
+const LoginUser = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+
+    return (
+        <section>
+            <form className="grid w-full max-w-sm items-center gap-3">
+                <p>Log into existing account</p>
+
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" onChange={(event) => setEmail(event.target.value)} placeholder="example@email.com" required />
+
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" onChange={(event) => setPassword(event.target.value)} required />
+            
+                <Button type="submit" variant="outline">Log in</Button>
+            </form>
+            <p>{message}</p>
+        </section>
+    );
+}
+
+export default LoginUser;
