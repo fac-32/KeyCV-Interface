f47029cf78ff8d2a3ec8848892a6ca79c8ed859b
Marina Shopina <marinashopina95@gmail.com>
2025-12-12 17:38:09 +0000

    feat: job form ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,20 @@ import { Routes, Route, Link } from "react-router";
 
 function App() {
   return (
-    <>
-      <Link to="/">Home</Link> | <Link to="/create-user">Create User</Link> |{" "}
-      <Link to="/login-user">Login</Link>
-      <Routes>
-        <Route path="/create-user" element={<CreateUser />} />
-        <Route path="/login-user" element={<LoginUser />} />
-        <Route path="/" element={<JobForm />} />
-      </Routes>
-    </>
+    <div className="app-shell">
+      <nav className="app-nav">
+        <Link to="/">Home</Link>
+        <Link to="/create-user">Create User</Link>
+        <Link to="/login-user">Login</Link>
+      </nav>
+      <main className="app-main">
+        <Routes>
+          <Route path="/create-user" element={<CreateUser />} />
+          <Route path="/login-user" element={<LoginUser />} />
+          <Route path="/" element={<JobForm />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
